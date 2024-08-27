c3c301e7dc1c8c281b4719a6c8d45d97746587a5
Pawan1618 <pawansinghshiva2030@gmail.com>
2024-08-27 20:14:13 +0530

    neocollab

---
+++ neocollab/bookname.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+/*
+important note 
+the cin.ignore( ) // ignore the newline 
+if you dont write this will not take input of string below */
+int main(){
+    int n;
+    cin>>n;
+    string arr[n];
+    cin.ignore();
+    for(int i=0;i<n;i++){
+        getline(cin,arr[i]);
+    }
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i];
+    // }
+    int n2;
+    cin>>n2;
+    string arr2[n2];
+    cin.ignore();
+    for(int i=0;i<n2;i++){
+        getline(cin,arr2[i]);
+    }
+}
