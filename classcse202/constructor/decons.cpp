e414cf7dd41b20a3a7f3245b2caa7fdfc9d8fb7d
Pawan1618 <pawansinghshiva2030@gmail.com>
2024-10-09 15:47:07 +0530

    kjh

--- classcse202/constructor/decons.cpp
+++ classcse202/constructor/decons.cpp
@@ -1,20 +1,46 @@
 #include<iostream>
 using namespace std;
-class counter{
+// class counter{
+//     private:
+//     int id;
+//     public:
+//     counter(int i){
+//         id=i;
+//         cout<<"constructor of obje with id"<<id<<endl;
+//     }
+//     ~counter(){
+//         cout<<"destructor wit id"<<id<<endl;
+//     }
+
+// };
+// // destruction invokation order is reverse 
+// int main(){
+//     counter c1(1);
+
+// }
+
+class RX{
     private:
-    int id;
+    int a;
     public:
-    counter(int i){
-        id=i;
-        cout<<"constructor of obje with id"<<id<<endl;
+    RX(int x){
+        a=x;
+        cout<<"h";
+        // cout<<"e"<<endl;
     }
-    ~counter(){
-        cout<<"destructor wit id"<<id<<endl;
+    void display(){
+        cout<<"construr is called "<<a<<endl;
     }
-
+    ~RX(){
+        cout<<"deconstructor is called "<<a<<endl;
+    }
 };
-// destruction invokation order is reverse 
 int main(){
-    counter c1(1);
+    RX obj1(10);
+    RX obj2(20);
+    RX obj3(1);
 
+    // obj2.display();
+    // obj3.display();
 }
+// destructor is called always 
