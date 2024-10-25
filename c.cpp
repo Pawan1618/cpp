2b439186ccaaf923a1989a7e544fe0a34e286afb
pawan <pawansinghshiva2030@gmail.com>
2024-10-25 16:54:19 +0530

    tyft

--- c.cpp
+++ c.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
+#include<iomanip>
+// #include<bits/stdc++.h>
+
 using namespace std;
 int main(){
-    float f=1.3;
-    cout<<f<<endl;
-    double d=1.32;
-    cout<< d << endl;
-    int size =sizeof(d);
-    cout<< "size of d is "<< size << endl;
+    // int n=1;
+    // // n=1;
+    // int n1=0, n2, n4;
+    // n2=3;
+    // int n3;
+    // cin>>n3>>n4;
+
+    // cout<<"n is "<<n<<n2<<n4 <<endl;
+    // cout<<endl<<"n3 is "<<n3<<"$"<<n1;
+
+    // float f=1.3;
+    // cout<<f<<endl;
+    // double d=1.32;
+    // cout<< d << endl;
+    // int size =sizeof(d);
+    // cout<< "size of d is "<< size << endl;
+    double n1;
+    n1=3.122;
+    cout<<fixed<<setprecision(1)<<n1;
+    // sort(n1,n3);
+    // scanf(%d,n);
 }
